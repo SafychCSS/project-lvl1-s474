f823a3261b88fc3057c327939fa6460e08290275
SafychCSS <safaraliev.renat@yandex.ru>
2019-04-10 12:08:16 +0300

    step 4

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 <a href="https://codeclimate.com/github/SafychCSS/project-lvl1-s474/"><img src="https://api.codeclimate.com/v1/badges/a99a88d28ad37a79dbf6/maintainability" /></a>
 [![Build Status](https://travis-ci.org/SafychCSS/project-lvl1-s474.svg?branch=master)](https://travis-ci.org/SafychCSS/project-lvl1-s474)
+
+[![asciicast](https://asciinema.org/a/qKS5WgXMlLq9lF392hVl1crRt.png)](https://asciinema.org/a/qKS5WgXMlLq9lF392hVl1crRt)
--- src/index.js
+++ src/index.js
@@ -6,4 +6,55 @@ const welcome = () => {
   console.log(`Hi, ${userName}!`);
 };
 
+const getRandomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+const hi = 'Welcome to the Brain Games!';
+const condition = 'Answer "yes" if number even otherwise answer "no".';
+
+export const gameEven = () => {
+  console.log(hi);
+  console.log(condition);
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
+  /*
+  const iter = (acc) => {
+    const randomNumber = getRandomNumber(1, 20);
+    const question = `Question: ${randomNumber}`;
+    if (acc === 3) {
+      console.log(`Congratulations, ${userName}!`);
+      return;
+    }
+    console.log(question);
+    const answer = readlineSync.question('Your answer: ');
+    const yes = randomNumber % 2 === 0 && answer === 'yes';
+    const no = randomNumber % 2 !== 0 && answer === 'no';
+    const correct = (yes || no);
+    if (correct) {
+      console.log('Correct!');
+      return iter(acc + 1);
+    }
+    console.log(`'yes' is wrong answer ;(. Correct answer was 'no'.Let's try again, ${userName}!`);
+  };
+  return iter(0);
+  */
+  let i = 1;
+  while (i < 4) {
+    const randomNumber = getRandomNumber(1, 20);
+    const question = `Question: ${randomNumber}`;
+    console.log(question);
+    const answer = readlineSync.question('Your answer: ');
+    const yes = randomNumber % 2 === 0 && answer === 'yes';
+    const no = randomNumber % 2 !== 0 && answer === 'no';
+    const correct = (yes || no);
+    if (correct) {
+      console.log('Correct!');
+      if (i === 3) {
+        console.log(`Congratulations, ${userName}!`);
+      }
+    } else {
+      console.log(`'yes' is wrong answer ;(. Correct answer was 'no'.Let's try again, ${userName}!`);
+      break;
+    }
+    i += 1;
+  }
+};
 export default welcome;
