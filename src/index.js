aaed8b903e10f57082f09f64df8550da642ce4e0
SafychCSS <safaraliev.renat@yandex.ru>
2019-04-10 15:32:15 +0300

    remove inner function iter

--- src/index.js
+++ src/index.js
@@ -16,27 +16,6 @@ export const gameEven = () => {
   console.log(condition);
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
-  /*
-  const iter = (acc) => {
-    const randomNumber = getRandomNumber(1, 20);
-    const question = `Question: ${randomNumber}`;
-    if (acc === 3) {
-      return console.log(`Congratulations, ${userName}!`);
-    }
-    console.log(question);
-    const answer = readlineSync.question('Your answer: ');
-    const isAnswer = (isEven(randomNumber) && answer === 'yes') ||
-    (!isEven(randomNumber) && answer === 'no');
-    if (isAnswer) {
-      console.log('Correct!');
-      return iter(acc + 1);
-    }
-    return console.log(`'yes' is wrong answer ;(.
-    Correct answer was 'no'.Let's try again, ${userName}!
-    `);
-  };
-  return iter(0);
-  */
   for (let i = 0; i < 3; i += 1) {
     const randomNumber = getRandomNumber(1, 20);
     const question = `Question: ${randomNumber}`;
