4ed97ecd93ce05d196b908bc01c2f2009e99e490
SafychCSS <safaraliev.renat@yandex.ru>
2019-04-11 11:02:14 +0300

    add file run-game.jss

--- src/games/calc.js
+++ src/games/calc.js
@@ -1,4 +1,5 @@
 import readlineSync from 'readline-sync';
+import runGame from '../run-game';
 import {
   getRandomNumber, hi, condition,
 } from '../const';
---
+++ src/run-game.js
@@ -0,0 +1,5 @@
+const runGame = () => {
+  // что-то нужно сделать общее для всех игр и экспорнуть
+};
+
+export default runGame;
