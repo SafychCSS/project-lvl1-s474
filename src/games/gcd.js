a9c9112a8a9e67d05c4e08f546e96da414577b7d
SafychCSS <safaraliev.renat@yandex.ru>
2019-04-11 23:49:35 +0300

    finished step 6 - gcd

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -1,21 +1,21 @@
-/*
 import runGame from '../run-game';
 import getRandomNumber from '../helpers';
 
 const condition = 'Find the greatest common divisor of given numbers.';
 
-const generateRound = () => {
-  const num1 = getRandomNumber(2, 100);
-  const num2 = getRandomNumber(2, 100);
-  console.log(`Question: ${num1} ${num2}`);
-  if (num1 > num2 && num1 % num2 === 0) {
-    return num2;
-  } else if (num1 < num2 && num2 % num1 === 0) {
-    return num1;
+const getGcd = (a, b) => {
+  if (b === 0) {
+    return a;
   }
-  const iter = (item1, item2, acc) => {
+  return getGcd(b, a % b);
+};
 
-  };
+const generateRound = () => {
+  const num1 = getRandomNumber(1, 10);
+  const num2 = getRandomNumber(1, 10);
+  const question = `${num1} ${num2}`;
+  const answer = String(getGcd(num1, num2));
+  const data = [question, answer];
+  return data;
 };
 export default () => runGame(condition, generateRound);
-*/
