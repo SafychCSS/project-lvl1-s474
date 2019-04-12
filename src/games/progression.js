5613f41648b25a2ebc6568e3e7b2748e5836d1a3
SafychCSS <safaraliev.renat@yandex.ru>
2019-04-12 22:00:09 +0300

    remove magic number 9

--- src/games/progression.js
+++ src/games/progression.js
@@ -3,11 +3,12 @@ import getRandomNumber from '../utils';
 
 const condition = 'What number is missing in the progression?';
 const countNumber = 10;
+const lastChangeNumber = 9;
 
 const generateRound = () => {
   const startNumber = getRandomNumber(1, 10);
   const step = getRandomNumber(1, 10);
-  const changeElementNumber = getRandomNumber(1, 9);
+  const changeElementNumber = getRandomNumber(1, lastChangeNumber);
 
   const generateProgression = () => {
     let nextNumberInList = startNumber + step;
