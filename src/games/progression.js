4cceaf04eeb6034bdd47cf7e188fa5bf6eaedf5e
SafychCSS <safaraliev.renat@yandex.ru>
2019-04-12 21:49:10 +0300

    change name variable

--- src/games/progression.js
+++ src/games/progression.js
@@ -2,26 +2,26 @@ import runGame from '../run-game';
 import getRandomNumber from '../utils';
 
 const condition = 'What number is missing in the progression?';
+const countNumber = 10;
 
 const generateRound = () => {
-  const beginProgression = getRandomNumber(1, 10);
-  const stepProgression = getRandomNumber(1, 10);
-  const lengthProgression = 10;
+  const startNumber = getRandomNumber(1, 10);
+  const step = getRandomNumber(1, 10);
   const changeElementNumber = getRandomNumber(1, 9);
 
   const generateProgression = () => {
-    let nextNumb = beginProgression + stepProgression;
-    const res = [beginProgression];
-    for (let i = 0; i < lengthProgression - 1; i += 1) {
+    let nextNumb = startNumber + step;
+    const res = [startNumber];
+    for (let i = 0; i < countNumber - 1; i += 1) {
       res.push(nextNumb);
-      nextNumb += stepProgression;
+      nextNumb += step;
     }
     return res;
   };
 
-  const progr = generateProgression();
-  const answer = String(progr.splice(changeElementNumber, 1, '...'));
-  progr.splice(changeElementNumber, 1, '...');
-  return [progr.join(' '), answer];
+  const listNumbers = generateProgression();
+  const answer = String(listNumbers.splice(changeElementNumber, 1, '...'));
+  listNumbers.splice(changeElementNumber, 1, '...');
+  return [listNumbers.join(' '), answer];
 };
 export default () => runGame(condition, generateRound);
