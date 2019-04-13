b50efcba096acc8188c6e6221f23e4267ede0ed3
SafychCSS <safaraliev.renat@yandex.ru>
2019-04-13 16:36:04 +0300

    Если выделена отдельная функция, то её нужно вынести на уровень модуля. - убрал функцию

--- src/games/calc.js
+++ src/games/calc.js
@@ -5,15 +5,10 @@ const condition = 'What is the result of the expression?';
 const operators = ['+', '-', '*'];
 
 const generateRound = () => {
-  const getOperationRandom = () => {
-    const getRandomIndex = getRandomNumber(0, operators.length - 1);
-    const getOperation = operators[getRandomIndex];
-    return getOperation;
-  };
-
   const operand1 = getRandomNumber(1, 10);
   const operand2 = getRandomNumber(1, 10);
-  const operation = getOperationRandom();
+  const index = getRandomNumber(0, operators.length - 1);
+  const operation = operators[index];
   let result = 0;
   switch (operation) {
     case '+':
