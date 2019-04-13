d7b3734cce7d22a5b57c55a630083502ecd8fcad
SafychCSS <safaraliev.renat@yandex.ru>
2019-04-13 16:12:17 +0300

    Не оставляйте лишних пустых строк

--- src/games/even.js
+++ src/games/even.js
@@ -4,7 +4,6 @@ import getRandomNumber from '../utils';
 const isEven = number => number % 2 === 0;
 const condition = 'Answer "yes" if number even otherwise answer "no".';
 
-
 const generateRound = () => {
   const question = getRandomNumber(1, 100);
   const answer = isEven(question) ? 'yes' : 'no';
