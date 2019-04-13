ea6421900206ac7e84fab030e7b779baa86a5fef
SafychCSS <safaraliev.renat@yandex.ru>
2019-04-13 15:51:58 +0300

    вынес функцию создания прогрессии на уровень модуля

--- src/games/progression.js
+++ src/games/progression.js
@@ -4,17 +4,17 @@ import getRandomNumber from '../utils';
 const condition = 'What number is missing in the progression?';
 const length = 10;
 
-const generateRound = () => {
+const generateProgression = () => {
   const step = getRandomNumber(1, length);
-  const changeElementNumber = getRandomNumber(1, length - 1);
+  const res = [];
+  for (let i = 0; i < length; i += 1) {
+    res.push(`${step}` * `${i}`);
+  }
+  return res;
+};
 
-  const generateProgression = () => {
-    const res = [];
-    for (let i = 0; i < length; i += 1) {
-      res.push(`${step}` * `${i}`);
-    }
-    return res;
-  };
+const generateRound = () => {
+  const changeElementNumber = getRandomNumber(1, length - 1);
 
   const listNumbers = generateProgression();
   const answer = String(listNumbers.splice(changeElementNumber, 1, '...'));
